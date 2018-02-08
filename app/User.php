b3c6733c0684c22ff6a252e0cc859fa6d98694ec
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-02-07 22:35:57 -0200

    Fit model to connect in MongoDB

--- app/User.php
+++ app/User.php
@@ -3,17 +3,14 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
-class User extends Authenticatable
+class User extends Eloquent
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    protected $connection = 'mongodb';
+    protected $collection = 'users';
     protected $fillable = [
         'name', 'email', 'password',
     ];
